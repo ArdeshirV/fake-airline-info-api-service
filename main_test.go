028778ced6836a8dd57ffb34a68938b741c3af9f
ArdeshirV <e-job@protonmail.com>
2023-07-06 11:11:48 +0330

    Correct main_test.go

--- main_test.go
+++ main_test.go
@@ -3,12 +3,12 @@ package main
 import (
 	"os"
 	"testing"
-	"github.com/the-go-dragons/fake-airline-info-service/config"
-	"github.com/the-go-dragons/fake-airline-info-service/server"
+	_ "github.com/the-go-dragons/fake-airline-info-service/config"
+	_ "github.com/the-go-dragons/fake-airline-info-service/server"
 )
 
 func TestMain(m *testing.M) {
-	os.Exit(testMain())
+	os.Exit(testMain(m))
 }
 
 func testMain(m *testing.M) int {
